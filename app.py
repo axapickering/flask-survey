05ee82141d7988e70a0968a1721b220fcea59510
Axa Pickering <axapickering@gmail.com>
2023-09-28 11:48:24 -0700

    trying to get answer to redirect to next question

--- app.py
+++ app.py
@@ -7,3 +7,23 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get("/")
+def home_page():
+    return render_template("survey_start.html",survey=survey)
+
+@app.post("/begin")
+def begin():
+    return redirect("/questions/0")
+
+@app.get("/questions/<int:question_number>")
+def display_question(question_number):
+    return render_template("/question.html",question=survey.questions[question_number],question_number=question_number)
+
+@app.post("/answer/<int:question_number>")
+def answer_to_question(question_number):
+    responses.append(request.form['answer'])
+    next_question = question_number + 1
+    return redirect('/question/{question_number+1}')
